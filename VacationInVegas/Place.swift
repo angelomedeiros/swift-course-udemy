ef90aa1227781dc0ed7404ccb6f396303b5164b5
Angelo Medeiros <angelomedeiros3@gmail.com>
2024-10-03 14:06:07 -0300

    feat: Lesson 3 - SwiftData

---
+++ VacationInVegas/Place.swift
@@ -0,0 +1,35 @@
+//
+//  Place.swift
+//  VacationInVegas
+//
+//  Created by Angelo Medeiros on 03/10/24.
+//
+
+import SwiftUI
+import MapKit
+import SwiftData
+
+@Model
+class Place {
+    #Unique<Place>([\.name, \.latitude, \.longitude])
+    
+    @Attribute(.unique) var name: String
+    var latitude: Double
+    var longitude: Double
+    var interested: Bool
+    
+    var location: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
+    var image: Image {
+        Image(name.lowercased().replacingOccurrences(of: " ", with: ""))
+    }
+    
+    init(name: String, latitude: Double, longitude: Double, interested: Bool) {
+        self.name = name
+        self.latitude = latitude
+        self.longitude = longitude
+        self.interested = interested
+    }
+}
