05ee567eff8aa521687d4fd3fc15815a85d1a93f
angelo.medeiros@govoll.com <angelomedeiros3@gmail.com>
2024-10-05 19:50:07 -0300

    feat: Lesson 6 - SwiftData Predicate

--- VacationInVegas/PlaceList.swift
+++ VacationInVegas/PlaceList.swift
@@ -12,10 +12,26 @@ struct PlaceList: View {
     @Query(sort: \Place.name) private var places: [Place]
     
     @State private var showImages = false
+    @State private var searchText = ""
+    @State private var filterByInterested = false
+    
+    private var predicate: Predicate<Place> {
+        #Predicate<Place> {
+            if !searchText.isEmpty && filterByInterested {
+                $0.name.localizedStandardContains(searchText) && $0.interested
+            } else if !searchText.isEmpty {
+                $0.name.localizedStandardContains(searchText)
+            } else if filterByInterested {
+                $0.interested
+            } else {
+                true
+            }
+        }
+    }
     
     var body: some View {
         NavigationStack {
-            List(places) { place in
+            List((try? places.filter(predicate)) ?? places) { place in
                 HStack {
                     place.image
                         .resizable()
@@ -40,10 +56,21 @@ struct PlaceList: View {
                         showImages.toggle()
                     }
                 }
+                
+                ToolbarItem(placement: .topBarLeading) {
+                    Button("Filter", systemImage: filterByInterested ? "star.fill" : "star") {
+                        withAnimation {
+                            filterByInterested.toggle()
+                        }
+                    }.tint(filterByInterested ? .yellow : .blue)
+                }
             }
             .sheet(isPresented: $showImages) {
                 Scrolling()
             }
+            .navigationTitle("Places")
+            .searchable(text: $searchText, prompt: "Find a place")
+            .animation(.default, value: searchText)
         }
     }
 }
